dbc6f883f61100c09efc13a8e28164728a404e05
Lahari Sure <laharisure@gmail.com>
2025-09-30 13:52:53 +0530

    Waits for dashboard header to confirm page load

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,5 +1,4 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -7,7 +6,21 @@ class DashboardPage:
     def __init__(self, driver):
         self.driver = driver
         self.pim_menu = (By.XPATH, "//span[text()='PIM']")
+        self.dashboard_header = (By.XPATH, "//h6[text()='Dashboard']")
 
     def go_to_pim(self):
-        pim = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.pim_menu))
-        ActionChains(self.driver).move_to_element(pim).click().perform()
+        # Wait for dashboard to fully load
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(self.dashboard_header)
+        )
+
+        try:
+            # Wait for PIM menu to be visible
+            pim = WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located(self.pim_menu)
+            )
+            pim.click()
+        except Exception as e:
+            print("Error locating or clicking PIM menu:", e)
+            print("Page source for debugging:\n", self.driver.page_source)
+            raise
